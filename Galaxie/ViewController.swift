b7788d8f53398aa4724777a10a4ac9a727995ef6
mohsylla <sylla.m7@gmail.com>
2022-07-18 16:35:06 +0200

    Initial Commit

---
+++ Galaxie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Galaxie
+//
+//  Created by Sylla on 18/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
